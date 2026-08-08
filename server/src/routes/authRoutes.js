9db74e01ea3a87afc0fd7cc5656ded83de957903
gs27304 <gajendrasingh27304@gmail.com>
2026-08-08 11:27:07 +0530

    Test auth router

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,6 +4,13 @@ import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.post('/register-test', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Auth router works'
+  });
+});
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', authMiddleware, logoutUser);
